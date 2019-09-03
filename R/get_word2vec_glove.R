988e0db83af1a5159c77edd469a7388852a1e5c0
Reagan Mozer <reaganmro@gmail.com>
2019-09-03 15:12:29 -0400

    Update get_word2vec_glove.R
    bug fix

--- R/get_word2vec_glove.R
+++ R/get_word2vec_glove.R
@@ -1,28 +1,3 @@
-#' This function calculates the Word2Vec embeddings
-#'
-#' @param dat FoxCNN corpus to calculate Word2Vec scores for
-#' @return A list of data frames containing the Word2Vec projections of the corpus
-#' @export
-
-get_word2vec_glove <- function(dir.source, corpus){
-  ## Read in the Fox/CNN data
-  tmp = list.files(paste0(getwd(),dir.source))
-  names.out = gsub(".txt", "", tmp)
-  for (j in 1:length(tmp)){
-  glove <- scan(file = paste0(getwd(),dir.source,tmp[j]), what="", sep="\n")
-  glove2 <- proc_pretrained_vec(glove)  # this is the actual function call
-  glove2 = t(glove2)
-  glove2= data.frame(glove2)
-  glove2$words = rownames(glove2)
-  k = ncol(glove2)
-  glove2 = glove2[,c(k, 1:(k-1))]
-  temp = softmaxreg::wordEmbed(corpus, dictionary=glove2, meanVec=TRUE)
-  assign(names.out[j], temp)
-  }
-  all=mget(names.out)
-  return(all)
-  }
-
 
 
 #' This function calculates the Word2Vec embeddings
@@ -49,6 +24,30 @@ proc_pretrained_vec <- function(p_vec) {
   glove <- data.frame(vals)
   names(glove) <- names
   return(glove)
+}
+
+#' This function calculates the Word2Vec embeddings
+#'
+#' @param dat FoxCNN corpus to calculate Word2Vec scores for
+#' @return A list of data frames containing the Word2Vec projections of the corpus
+#' @export
+get_word2vec_glove <- function(dir.source, corpus){
+  ## Read in the Fox/CNN data
+  tmp = list.files(paste0(getwd(),dir.source))
+  names.out = gsub(".txt", "", tmp)
+  for (j in 1:length(tmp)){
+    glove <- scan(file = paste0(getwd(),dir.source,tmp[j]), what="", sep="\n")
+    glove2 <- proc_pretrained_vec(glove)  # this is the actual function call
+    glove2 = t(glove2)
+    glove2= data.frame(glove2)
+    glove2$words = rownames(glove2)
+    k = ncol(glove2)
+    glove2 = glove2[,c(k, 1:(k-1))]
+    temp = softmaxreg::wordEmbed(corpus, dictionary=glove2, meanVec=TRUE)
+    assign(names.out[j], temp)
+  }
+  all=mget(names.out)
+  return(all)
 }
 
 
