8ee50aa11cacad28d8b11ae868484432ed9865b5
Reagan Mozer <reaganmro@gmail.com>
2020-04-29 10:40:04 -0400

    Update dependencies

---
+++ R/tada.R
@@ -0,0 +1,31 @@
+#' Extract an array of numerical features from a collection of text documents
+#' 
+#' @import quanteda
+#' @import tm
+#' @param text a character vector of text documents
+#' @param control a named list of control options for evaluating documents individually and globally
+#' @return a \code{data.frame} representation of text with columns corresponding to text-based features
+#' @export
+
+
+tada <- function(text, lower=TRUE, alphaNum=TRUE, stopwords=NULL, stem=FALSE){
+  
+  stopifnot(is.character(text))
+  
+  
+  if (alphaNum){
+    text = iconv(text, from="UTF-8", to="ASCII//TRANSLIT", sub="XX")
+    text = gsub("[^[:alnum:][:blank:]+.,'?!:;&/\\-]", "", text)
+  }
+  if (lower){
+    text = tolower(text)
+  }
+  if (!is.null(stopwords)){
+    text = tm::removeWords(text,stopwords)
+  }
+  
+  text
+  
+  
+}
+  
